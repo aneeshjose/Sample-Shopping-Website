91deee0cf81096b9ba373fd29cd130569b77aee0
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2021-01-08 21:15:10 +0530

    fix: get cart if user not present

--- cart.py
+++ cart.py
@@ -1,12 +1,13 @@
 from flask import session, make_response, request, render_template
 from user import User
+from json import dumps, loads
 
 
 class Cart:
     def addToCart(self, dbHelper, prodId):
 
         try:
-            session['email']
+            email = session['email']
             # since the email is nol null, check the user session
             # also matches the user session auth_key
             User().checkUser(dbHelper)
@@ -15,42 +16,84 @@ class Cart:
             #  if added, increment the count by 1
             count = dbHelper.query(
                 'select count(*) from cart where userid=\'{}\' and productid=\'{}\''
-                            .format(session['email'], prodId))
+                            .format(email, prodId))
             if count[0][0] == 0:
                 dbHelper.query('insert into cart values(\'{}\',\'{}\',{})'
-                               .format(session['email'], prodId, 1))
+                               .format(email, prodId, 1))
                 return 'success'
             dbHelper.query(
                 'update cart set count=count+1 where userid=\'{}\' and productid=\'{}\''
-                .format(session['email'], prodId))
+                .format(email, prodId))
             return 'success'
         except:
             # The user has not signed in yet.
             # So add the product id to the cookie in comma seperated format
             # if there are no available cookies for this, creating a new one
             available = request.cookies.get('cart')
-            if available == None:
-                available = ''
+            # creating local cart datastructure in the format {productid:count}
+            prods = loads(available)
+            try:
+                prods[prodId] += 1
+            except:
+                prods[prodId] = 1
             resp = make_response()
-            resp.set_cookie('cart', available+','+prodId)
-            return 'success'
+            resp.set_cookie('cart', dumps(prods))
+            return resp
 
     def subtractcart(self, dbHelper, id):
-        dbHelper.query("update cart set count=count-1 where userid='{}' and productid='{}'"
-                       .format(session['email'], id))
+        try:
+            email = session['email']
+            if dbHelper.query(
+                    "select * from cart where userid='{}' and productid='{}'".format(email, id))[0][0] == 1:
+                return False
+            dbHelper.query("update cart set count=count-1 where userid='{}' and productid='{}'"
+                           .format(email, id))
+            return True
+        except:
+            available = request.cookies.get('cart')
+            # creating local cart datastructure in the format {productid:count}
+            prods = loads(available)
+            try:
+                if prods[id] == 1:
+                    return True
+                prods[id] -= 1
+            except:
+                pass
+            resp = make_response()
+            resp.set_cookie('cart', dumps(prods))
+            return resp
         return 'success'
 
     def removeFromCart(self, dbHelper, id):
-        dbHelper.query("delete from cart where userid='{}' and productid='{}'"
-                       .format(session['email'], id))
+        try:
+            dbHelper.query("delete from cart where userid='{}' and productid='{}'"
+                           .format(session['email'], id))
+        except:
+            available = request.cookies.get('cart')
+            prods = loads(available)
+            del prods[id]
+            resp = make_response()
+            resp.set_cookie('cart', dumps(prods))
+            return resp
         return 'success'
 
     def displayCart(self, dbHelper):
-        email = session['email']
-        cartProducts = dbHelper.query('select productid,count from cart where userid=\'{}\''
-                                      .format(email))
+        try:
+            email = session['email']
+
+            self.cartProducts = dbHelper.query('select productid,count from cart where userid=\'{}\''
+                                               .format(email))
+        except:
+            cart = request.cookies.get('cart')
+            try:
+                self.prods = loads(cart)
+            except:
+                self.prods = {}
+            self.cartProducts = [(id, count)
+                                 for id, count in self.prods.items()]
+            # self.cartProducts =
         prodEnlarged = []
-        for id in cartProducts:
+        for id in self.cartProducts:
             prodEnlarged.append({'item': dbHelper.query(
                 "select * from products where id='{}'".format(id[0])), 'count': id[1]})
         return render_template('cart.html', products=prodEnlarged)
